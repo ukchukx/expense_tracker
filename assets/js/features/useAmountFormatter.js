6851cc99c0a5ca10f379ad76d04928d8afdcea7a
Uk Chukundah <ukchukundah@gmail.com>
2022-04-01 17:11:08 +0200

    Fix currency symbol when SEK is selected

--- assets/js/features/useAmountFormatter.js
+++ assets/js/features/useAmountFormatter.js
@@ -4,7 +4,11 @@ export default function useAmountFormatter() {
   const amountFormatter = (amount, currency = 'NGN') => displayAmount(amount, currency);
   const koboAmountToNaira = (amount) => (amount / 100).toFixed(2);
   const formatKoboAmount = (amount, currency = 'NGN') => amountFormatter(koboAmountToNaira(amount), currency);
-  const currencySymbol = (currency = 'NGN') => displayAmount(1, currency)[0];
+  const currencySymbol = (currency = 'NGN') => {
+    const str = displayAmount(1, currency).match(/\D+/g);
+
+    return currency === 'SEK' ? str[1] : str[0];
+  }
 
   return { amountFormatter, formatKoboAmount, koboAmountToNaira, currencySymbol };
 }
